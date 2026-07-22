1a32b92f97aad731ae41b9bc3c4eaf67db01bdeb
bkb60316-netizen <bkb60316@gmail.com>
2026-07-22 22:01:41 +0530

    Implement message handler for greetings and responses

--- handlers/message.py
+++ handlers/message.py
@@ -1,1 +1,41 @@
+from telegram import Update
+from telegram.ext import ContextTypes
 
+from services.logger import logger
+
+
+async def message_handler(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE,
+):
+
+    user_message = update.message.text.strip()
+
+    logger.info(
+        f"Message Received: {user_message}"
+    )
+
+    greetings = [
+        "hi",
+        "hello",
+        "hey",
+        "hii",
+        "good morning",
+        "good evening",
+        "good night",
+    ]
+
+    if user_message.lower() in greetings:
+
+        await update.message.reply_text(
+            "👋 Hello!\n\n"
+            "मैं Study Helper AI हूँ।\n"
+            "आप अपना कोई भी Study Question पूछ सकते हैं।"
+        )
+
+        return
+
+    await update.message.reply_text(
+        "🤖 AI System अभी तैयार किया जा रहा है।\n\n"
+        "बहुत जल्द मैं आपके सभी सवालों का जवाब दूँगा।"
+      )
