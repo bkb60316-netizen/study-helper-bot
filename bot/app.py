6492118eaf1310eec8d340aae7398018db1ece59
bkb60316-netizen <bkb60316@gmail.com>
2026-07-24 07:50:34 +0530

    Add new command handlers for about, language, and settings

--- bot/app.py
+++ bot/app.py
@@ -11,6 +11,9 @@ from services.logger import logger
 from handlers.start import start
 from handlers.help import help_command
 from handlers.message import message_handler
+from handlers.about import about_command
+from handlers.language import language_command
+from handlers.settings import settings_command
 
 
 def create_application() -> Application:
@@ -23,6 +26,9 @@ def create_application() -> Application:
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("about", about_command))
+    application.add_handler(CommandHandler("language", language_command))
+    application.add_handler(CommandHandler("settings", settings_command))
 
     application.add_handler(
         MessageHandler(
